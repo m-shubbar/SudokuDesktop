c3e7903169bff2312cbd3d62f6d4fbd5090cd7c5
Mustafa <codingbox@outlook.com>
2021-08-02 19:51:55 -0400

    adding interfaces

---
+++ src/ca/shubbar/Main.java
@@ -0,0 +1,13 @@
+package ca.shubbar;
+
+import ca.shubbar.gamebox.SudokuGame;
+
+/**
+ * @author Mustafa <codingbox@outlook.com>
+ * Created at 2021-08-02
+ */
+public class Main {
+    public static void main(String[] args){
+        SudokuApp.main(new String[]{});
+    }
+}
---
+++ src/ca/shubbar/userinterface/IUserInterfaceContract.java
@@ -0,0 +1,22 @@
+package ca.shubbar.userinterface;
+
+import ca.shubbar.gamebox.SudokuGame;
+
+/**
+ * @author Mustafa <codingbox@outlook.com>
+ * Created at 2021-08-02
+ */
+public interface IUserInterfaceContract {
+    interface EventListener {
+        void onSudokuInput(int x, int y, int input);
+        void onDialogClick();
+    }
+
+    interface View{
+        void setListener(IUserInterfaceContract.EventListener listener);
+        void updateSquare(int x, int y, int input);
+        void updateBoard(SudokuGame game);
+        void showDialog(String message);
+        void showError(String message);
+    }
+}
