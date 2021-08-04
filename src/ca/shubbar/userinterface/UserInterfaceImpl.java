9311c45fcd20d6a84479e922727c903758b0ab25
Mustafa <codingbox@outlook.com>
2021-08-03 23:29:09 -0400

    board, tile and textfields interface

--- src/ca/shubbar/userinterface/UserInterfaceImpl.java
+++ src/ca/shubbar/userinterface/UserInterfaceImpl.java
@@ -3,9 +3,13 @@ package ca.shubbar.userinterface;
 import ca.shubbar.gamebox.Coordinates;
 import ca.shubbar.gamebox.SudokuGame;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.Background;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.util.HashMap;
@@ -47,9 +51,117 @@ public class UserInterfaceImpl implements IUserInterfaceContract.View, EventHand
         stage.show();
     }
 
+    private void drawGridLines(Group root) {
+        int xAndY = 114;
+        int index = 0;
+        while(index < 8) {
+            int thickness;
+            if(index == 2 || index == 5) {
+                thickness = 3;
+            } else {
+                thickness = 2;
+            }
+            Rectangle verticalLine = getLine(
+                    xAndY + 64 * index,
+                    BOARD_PADDING,
+                    BOARD_X_AND_Y,
+                    thickness
+            );
+
+            Rectangle horizontalLine = getLine(
+                    BOARD_PADDING,
+                    xAndY + 64 * index,
+                    thickness,
+                    BOARD_X_AND_Y
+            );
+
+            root.getChildren().addAll(
+                    verticalLine,
+                    horizontalLine
+            );
+
+            index++;
+        }
+    }
+
+    private Rectangle getLine(double x,
+                              double y,
+                              double height,
+                              double width) {
+
+
+        Rectangle line = new Rectangle();
+        line.setX(x);
+        line.setY(y);
+        line.setHeight(height);
+        line.setWidth(width);
+        line.setFill(Color.BLACK);
+
+        return line;
+    }
+
+    private void drawTextFields(Group root) {
+        final int xOrigin = 50;
+        final int yOrigin = 50;
+
+        final int xAndYDelta = 64;
+
+        // O(n^2)
+        for(int xIndex = 0; xIndex < 9; xIndex++) {
+            for(int yIndex = 0; yIndex < 9; yIndex++) {
+                int x = xOrigin + xIndex * xAndYDelta;
+                int y = yOrigin + yIndex * xAndYDelta;
+
+                SudokuTextField tile = new SudokuTextField(xIndex, yIndex);
+
+                styleSudokuTile(tile, x, y);
+
+                tile.setOnKeyPressed(this);
+                textFieldCoordinates.put(new Coordinates(xIndex, yIndex), tile);
+
+                root.getChildren().add(tile);
+            }
+        }
+    }
+
+    private void styleSudokuTile(SudokuTextField tile, double x, double y) {
+        Font numberFont = new Font(32);
+        tile.setFont(numberFont);
+        tile.setAlignment(Pos.CENTER);
+
+        tile.setLayoutX(x);
+        tile.setLayoutY(y);
+
+        tile.setPrefHeight(64);
+        tile.setPrefWidth(64);
+
+        tile.setBackground(Background.EMPTY);
+    }
+
+    private void drawSudokuBoard(Group root) {
+        Rectangle boardBackground = new Rectangle();
+        boardBackground.setX(BOARD_PADDING);
+        boardBackground.setY(BOARD_PADDING);
+
+        boardBackground.setHeight(BOARD_X_AND_Y);
+        boardBackground.setWidth(BOARD_X_AND_Y);
+
+        boardBackground.setFill(WINDOW_BOARD_COLOR);
+
+        root.getChildren().addAll(boardBackground);
+    }
+
+    private void drawTitle(Group root) {
+
+    }
+
+    private void drawBackground(Group root) {
+
+    }
+
     @Override
     public void setListener(IUserInterfaceContract.EventListener listener) {
-
+        this.listener = listener;
     }
 
     @Override
